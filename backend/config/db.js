aabd1546ed63d843b30ec443391f37f04389cbf7
adithyan-asam <adithyan4asam@gmail.com>
2025-08-16 15:56:33 +0530

    commit

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,11 +2,10 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'news_app',
-    password: 'asam@2005',
-    port: 5432,
+    connectionString: process.env.DB_URL,
+    ssl: {
+        rejectUnauthorized: false // required for Render
+    }
 });
 
 module.exports = pool;
